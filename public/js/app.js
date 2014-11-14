16cc6ecf9ee092e46d5569ba320b0a77ef915355
Hassen <chaieb.hassen@yahoo.com>
2014-11-13 19:49:52 -0800

    todo bug changeState

--- public/js/app.js
+++ public/js/app.js
@@ -1,120 +1,133 @@
 var app = angular.module('airline', ['airlineServices', 'airlineFilters', 'ui.router', 'angularTreeview']);
 
+app.directive('applyChange', function() {
+    return {
+        link: function(scope, element, attrs) {
+            scope.$watch('abc.currentNode', function(newValue) {
+                if (newValue) {
+                    for (var i = 0; i < newValue.element.attributes.length; i++) {
+                        scope.$watchCollection('abc.currentNode.element.attributes[' + i + ']', function(newAttribute) {
+                            if (newAttribute) {
+                                newValue.element[scope.attributesName][newAttribute.name] = newAttribute.value;
+                            }
+                        });
+
+                    };
+                }
+            });
+        }
+    }
+});
 
 
 app.directive('importSvg', function() {
-  return {
-    templateUrl: function(elem, attrs) {
-      return attrs.templateUrl || '/img/contactSmiley-5.svg'
-    },
-    link: function(scope, element, attrs) {
-      console.log("eeeeeee");
-      var allSmileys = element[0].parentNode.children;
-      var svgImage = element[0].children[0];
-      var gObject = d3.select(svgImage);
-      gObject.style("width", "45px");
-      scope.svg = gObject[0];
+    return {
+        templateUrl: function(elem, attrs) {
+            return attrs.templateUrl || '/img/contactSmiley-5.svg'
+        },
+        link: function(scope, element, attrs) {
+            var allSmileys = element[0].parentNode.children;
+            var svgImage = element[0].children[0];
+            var gObject = d3.select(svgImage);
+            gObject.style("width", "45px");
+            scope.svg = gObject[0];
 
-      function create(a) {
-        var obj = {};
-        obj.element = a;
-        obj.name = a.id + ":" + a.tagName;
-        obj.children = [];
-        for (var i = 0; i < a.children.length; i++) {
-          obj.children.push(create(a.children[i]));
-        };
-        return obj
-      };
+            function create(a) {
+                var obj = {};
+                obj.element = a;
+                obj.name = a.id + ":" + a.tagName;
+                obj.children = [];
+                for (var i = 0; i < a.children.length; i++) {
+                    obj.children.push(create(a.children[i]));
+                };
+                return obj
+            };
 
-      function attributesOfObject(a) {
-        obj = {};
-        for (var i = 0; i < a.attributes.length; i++) {
-          obj[a.attributes[i].name] = a.attributes[i].value;
-        };
-        return obj;
-      };
-
-
-      function createState2(a, stateNumber) {
-
-        a["attributes_" + stateNumber] = [];
-        for (var i = 0; i < a.attributes.length; i++) {
-          a["attributes_" + stateNumber][i] = {};
-          a["attributes_" + stateNumber][i].name = a.attributes[i].name;
-          a["attributes_" + stateNumber][i].value = a.attributes[i].value;
-        };
-        for (var i = 0; i < a.children.length; i++) {
-          createState(a.children[i], stateNumber);
-        };
-      };
-
-      function createState(a, stateNumber) {
-
-        a["attributes_" + stateNumber] = {};
-        for (var i = 0; i < a.attributes.length; i++) {
-          if (a.attributes[i].prefix === null) {
-            a["attributes_" + stateNumber][a.attributes[i].name] = a.attributes[i].value;
-          }
-        };
-        for (var i = 0; i < a.children.length; i++) {
-          createState(a.children[i], stateNumber);
-        };
-      };
+            function attributesOfObject(a) {
+                obj = {};
+                for (var i = 0; i < a.attributes.length; i++) {
+                    obj[a.attributes[i].name] = a.attributes[i].value;
+                };
+                return obj;
+            };
 
 
+            function createState2(a, stateNumber) {
 
-      createState(gObject[0][0], 1);
-      createState(gObject[0][0], 2);
-      console.log(gObject[0][0]["attributes_2"]);
-      gObject[0][0]["attributes_2"].opacity = "0.3";
+                a["attributes_" + stateNumber] = [];
+                for (var i = 0; i < a.attributes.length; i++) {
+                    a["attributes_" + stateNumber][i] = {};
+                    a["attributes_" + stateNumber][i].name = a.attributes[i].name;
+                    a["attributes_" + stateNumber][i].value = a.attributes[i].value;
+                };
+                for (var i = 0; i < a.children.length; i++) {
+                    createState(a.children[i], stateNumber);
+                };
+            };
 
-      // d3.select(gObject[0][0]).attr(gObject[0][0]["attributes_2"]);
-      // d3.select(gObject[0][0]).attr({"xmlns:xlink": "http://www.w3.org/1999/xlink"});
+            function createState(a, stateNumber) {
 
-      // console.log(gObject[0][0].children[3].children[0]["attributes_" + 2]);
+                a["attributes_" + stateNumber] = {};
+                for (var i = 0; i < a.attributes.length; i++) {
+                    if (a.attributes[i].prefix === null) {
+                        a["attributes_" + stateNumber][a.attributes[i].name] = a.attributes[i].value;
+                    }
+                };
+                for (var i = 0; i < a.children.length; i++) {
+                    createState(a.children[i], stateNumber);
+                };
+            };
 
-      scope.svg = [create(gObject[0][0])];
-      console.log(scope.svg);
-
-      d3.select(scope.svg[0].element).attr(scope.svg[0]["attributes_2"]);
 
 
-      console.log(gObject[0][0]);
-      console.log(gObject[0][0].children[3].children);
-      console.log(gObject[0][0].children[3].children[0]);
-      gObject[0][0].children[3].children[0].attributes_2 = {};
-      // d3.select(gObject[0][0].children[3].children[0]).attr(attributesFromObject(gObject[0][0].children[3].children[0]));
+            createState(gObject[0][0], 1);
+            createState(gObject[0][0], 2);
+            // console.log(gObject[0][0]["attributes_2"]);
+            // gObject[0][0]["attributes_2"].opacity = "0.3";
+
+            // d3.select(gObject[0][0]).attr(gObject[0][0]["attributes_2"]);
+            // d3.select(gObject[0][0]).attr({"xmlns:xlink": "http://www.w3.org/1999/xlink"});
+
+            // console.log(gObject[0][0].children[3].children[0]["attributes_" + 2]);
+
+            scope.svg = [create(gObject[0][0])];
+            console.log(scope.svg);
+            // console.log(gObject[0][0]);
+            // console.log(gObject[0][0].children[3].children);
+            // console.log(gObject[0][0].children[3].children[0]);
+            // gObject[0][0].children[3].children[0].attributes_2 = {};
+            // d3.select(gObject[0][0].children[3].children[0]).attr(attributesFromObject(gObject[0][0].children[3].children[0]));
+        }
     }
-  }
 });
 
 
 app.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', '$locationProvider',
-  function($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider) {
-    $stateProvider.
-    state('home', {
-      url: '/',
-      templateUrl: 'partials/import.html',
-      controller: 'ImportCtrl',
-      // onEnter: ['$state', 'Account', function($state, Account) {
-      //     if (Account.hasRole('user')) {
-      //         $state.go('profile');
-      //     };
-      // }]
-    });
+    function($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider) {
+        $stateProvider.
+        state('home', {
+            url: '/',
+            templateUrl: 'partials/import.html',
+            controller: 'ImportCtrl',
+            // onEnter: ['$state', 'Account', function($state, Account) {
+            //     if (Account.hasRole('user')) {
+            //         $state.go('profile');
+            //     };
+            // }]
+        });
 
-    $locationProvider.html5Mode(true); // remove the ugly # in the url
-  }
+        $locationProvider.html5Mode(true); // remove the ugly # in the url
+    }
 ]);
 
 angular.module('airlineFilters', [])
-  .filter('originTitle', function() {
-    return function(input) {
-      return input.origin + ' - ' + input.originFullName;
-    };
-  })
-  .filter('destinationTitle', function() {
-    return function(input) {
-      return input.destination + ' - ' + input.destinationFullName;
-    };
-  });
+    .filter('originTitle', function() {
+        return function(input) {
+            return input.origin + ' - ' + input.originFullName;
+        };
+    })
+    .filter('destinationTitle', function() {
+        return function(input) {
+            return input.destination + ' - ' + input.destinationFullName;
+        };
+    });
--- public/js/controllers/import.js
+++ public/js/controllers/import.js
@@ -3,27 +3,36 @@ app.controller('ImportCtrl', ['$scope', 'Airport',
 
         $scope.svg = {};
         $scope.attributesName = "attributes_1";
+        $scope.previousAttributesName = "attributes_1";
         $scope.state = function(stateNumber) {
+            $scope.previousAttributesName = $scope.attributesName;
             $scope.attributesName = "attributes_" + stateNumber;
             $scope.applyNewAttributes($scope.svg[0]);
-            console.log("done")
+            console.log("done");
         };
 
 
         $scope.applyNewAttributes = function(a) {
+            // el.element.attributes={}
             var el = a;
             d3.select(el.element).attr(el.element[$scope.attributesName]);
             for (var i = 0; i < el.children.length; i++) {
-                $scope.applyAttributes(el.children[i]);
+                $scope.applyNewAttributes(el.children[i]);
             };
         };
 
 
-        $scope.applyChange = function() {
-            scope.$watch('svg', function(newValue, oldValue) {
-              console.log(newValue, oldValue);
-            });
+        $scope.animateState = function(a,state1,state2) {
+            var el = a;
+            // console.log(el.element[$scope.previousAttributesName],el.element["attributes_" + stateNumber]);
+            d3.select(el.element).attr(el.element["attributes_" + state1]).transition().attr(el.element["attributes_" + state2]);
+            for (var i = 0; i < el.children.length; i++) {
+                $scope.animateState(el.children[i],state1,state2);
+            };
         };
+
+        //todo reload the value in the form
+
 
 
         // $scope.attributesOfObject= function(a) {
